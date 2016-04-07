b300a934339d6874fcaa49412228c94afb433ff9
Bill Evans <bill@8pawexpress.com>
2016-04-07 13:07:03 -0700

    axis, xlab, labels, data.frame drop, etc
    - new: axis.args
    - new: return value breaks "at" and "label" down by factor/subfactor
    - new: xlab for each factor label
    - changed: do not try to plot densities if width is 0
    - changed: groups is SIGNIFICANTLY DIFFERENT (better)
    - changed: modify grid (abd abline) to intercept grid(nx) and draw
      vertical lines at each density plot
    - changed: split sep from default (".") to "|"
    - fixed: data.frame subsetting was dropping to a vector if ncol 1
    - fixed: lines were not counting correctly
    - fixed: order is preserved in the output lists
    - fixed: xlim not influenced by boxplot boxwex

--- R/violinplot.R
+++ R/violinplot.R
@@ -50,6 +50,9 @@
 #'   violin plots. As with \code{col}, the values in \code{border} are
 #'   recycled. Note: does not affect boxplots, use \code{boxplot =
 #'   list(border = c(...))} to change them.
+#' @param xlab character vector, names of the levels to plot; by
+#'   default (if \code{NULL}), these will be derived from the data
+#'   (e.g., column or variable names).
 #' @param groups for internal use only (calls between methods), a list
 #'   for assigning groups and levels of the data. (Internally, the
 #'   structure is a list of lists, where each element of the top-level
@@ -213,13 +216,15 @@ violinplot.formula <- function(formula, data = NULL, ...,
   m$... <- NULL
   m$na.action <- na.action
   m[[1L]] <- quote(stats::model.frame)
+
+  rhsonly <- length(formula) == 2L
+
   mf <- eval(m, parent.frame())
-  response <- attr(attr(mf, "terms"), "response")
+  # response <- attr(attr(mf, "terms"), "response")
+  if (ncol(mf) > 1) mf <- mf[ do.call("order", as.list(mf[,-1,drop = FALSE])), ]
 
   dots <- list(...)
   dots <- dots[ names(dots) != '' ]
-
-  rhsonly <- length(formula) == 2L
 
   spl <- 
     if (rhsonly) {
@@ -227,14 +232,14 @@ violinplot.formula <- function(formula, data = NULL, ...,
       mf
     } else {
       # LHS and RHS of formula
-      split(mf[,1], mf[,-1], drop = drop.unused)
+      split(mf[,1], mf[,-1], drop = drop.unused, sep = "|")
     }
   
   numfctrs <- if (rhsonly) ncol(mf) else rev(lengths(lapply(mf[,-1,drop = FALSE], unique)))
   nf <- length(numfctrs)
   prodf <- prod(numfctrs)
 
-  sn <- lapply(strsplit(names(spl), '\\.'), rev)
+  sn <- lapply(strsplit(names(spl), '\\|'), rev)
   foo1 <-
     if (ncol(mf) == 1) {
       1
@@ -258,12 +263,21 @@ violinplot.formula <- function(formula, data = NULL, ...,
   at <- cumsum(c(if (rhsonly && ncol(mf) == 1) NULL else 1,
                  dots$group.spacers[ foo1 ]))
 
+  # this alone will work, but the order is natural, not factor-based
+  # (if factors)
   groups <- lapply(1:max(lengths(sn)), function(i) {
     first <- sapply(sn, `[[`, i)
-    sapply(split(seq_along(first), first), function(is) {
-      lapply(split(is, cumsum(c(1, diff(is)) != 1)), function(j) at[j])
-    }, simplify = FALSE)
+    # the added [first] ensures it stays in the right order
+    x <- split(seq_along(first), first)[first]
+    sapply(split(x, names(x)), function(a) unlist(unique(a)), simplify = FALSE)
   })
+
+  # this reorders per factors (if factors)
+  groups2 <- mapply(function(grp, orig) {
+    lvls <- levels(orig)
+    if (is.null(lvls)) grp else grp[lvls]
+  }, groups, rev(as.list(mf[,if (rhsonly) TRUE else -1, drop = FALSE])), SIMPLIFY = FALSE)
+
   names(groups) <- if (rhsonly) NULL else names(numfctrs)
   
   if (! "ylab" %in% names(dots)) {
@@ -278,12 +292,16 @@ violinplot.formula <- function(formula, data = NULL, ...,
 #' @export
 violinplot.default <- function(x, ..., at = NULL, width = 1, names = NULL,
                         col = NULL, border = 1,
-                        groups = NULL, group.spacers = 1,
+                        xlab = NULL, groups = NULL, group.spacers = 1,
                         group.labels = NULL, group.lines = TRUE,
                         density.auto = TRUE, density.scale = TRUE, density.ticks = TRUE,
-                        density.args = NULL, boxplot.args = TRUE, legend.args = NULL,
+                        density.args = NULL, boxplot.args = TRUE,
+                        legend.args = NULL, axis.args = TRUE,
                         grid.args = TRUE, text.cex = 1,
                         mar = FALSE, add = FALSE) {
+
+  # TODO: group.lines, group.labels vector of bool
+
   args <- list(x, ...)
   nms <- names(args)
   if (is.null(nms)) nms <- rep("", length(args))
@@ -301,6 +319,7 @@ violinplot.default <- function(x, ..., at = NULL, width = 1, names = NULL,
   defdensityargs <- list(n = 32L)
   defgridargs <- list(nx = NA, ny = NULL, col = "gray", lty = "dotted", lwd = 0.2)
   deflegendargs <- list(x = "topleft", xpd = TRUE, horiz = TRUE)
+  defaxisargs <- list(side = 2, las = 1, cex.axis = text.cex)
   defboxplotargs <- list(pars = list(boxwex = 0.05, staplewex = 0, outwex = 0.5),
                          # the following options are to prevent boxplot()
                          # from plotting anything other than the actual boxplots
@@ -330,18 +349,28 @@ violinplot.default <- function(x, ..., at = NULL, width = 1, names = NULL,
       }
   }
 
-  if (is.null(groups)) {
+  if (is.null(groups)) {                # FIXME: test without formula    
     groups <- lapply(1:max(lengths(names)), function(i) {
       sapply(split(seq_along(names), names), function(is) {
         lapply(split(is, cumsum(c(1, diff(is)) != 1)), function(j) at[j])
       }, simplify = FALSE)
     })
   }
+  ngrps <- length(groups)
+
+  if (! is.null(xlab)) {
+    if (length(xlab) < ngrps) {
+      xlab <- c(xlab, rep("", ngrps - length(xlab)))
+    }
+    groupnames <- xlab
+  } else {
+    groupnames <- names(groups)
+  }
 
   if (missing(at)) at <- 1
   # "just above" 0.5 in order to contain the plot that should go out
   # to 0.5 at some point
-  xrange <- width * c(-0.51, 0.51) + range(at)
+  xrange <- (if (width > 0) width else boxplot.args$pars$boxwex) * c(-0.51, 0.51) + range(at)
   yrange <- range(sapply(dens, function(d) {
     if (d$n == 0) NA else range(d$x, na.rm = TRUE)
   }), na.rm = TRUE)
@@ -352,10 +381,7 @@ violinplot.default <- function(x, ..., at = NULL, width = 1, names = NULL,
       }), na.rm = TRUE)
     } else 1
 
-  if (is.null(group.labels)) {
-    l <- length(groups)
-    group.labels <- if (l > 1) l else 1
-  }
+  if (is.null(group.labels)) group.labels <- rep(TRUE, ngrps)
 
   mainmar <- if (! is.null(dots$main)) 3 else 0
 
@@ -363,11 +389,17 @@ violinplot.default <- function(x, ..., at = NULL, width = 1, names = NULL,
     legtopmar <- legbotmar <- 0
   } else {
     # legend.args <- list()
-    deflabels <- names(groups[[ length(groups) ]])
-    group.labels <- min(group.labels, length(groups) - 1)
+    # FIXME! should not need unique
+    deflabels <- unique(names(groups[[ ngrps ]]))
+    group.labels[ngrps] <- FALSE
+    if (is.logical(legend.args)) {
+      legend.args <- if (legend.args) deflegendargs else NULL
+    }
     if (is.character(legend.args)) {
       legpos <- legend.args
       legtext <- deflabels
+      legend.args <- deflegendargs
+      deflegendargs$x <- NULL
     } else if (is.list(legend.args)) {
       legpos <- if ("x" %in% names(legend.args)) legend.args$x else "bottomleft"
       legend.args <- legend.args[ ! names(legend.args) %in% c('x', 'legend') ]
@@ -379,8 +411,10 @@ violinplot.default <- function(x, ..., at = NULL, width = 1, names = NULL,
             legend.args$legend
           }
         } else deflabels
-    } # if (is.character(legend.args)) ... else
-    if (grepl("top", legpos)) {
+    }
+    if (is.null(legend.args)) {
+      legtopmar <- legbotmar <- 0
+    } else if (grepl("top", legpos)) {
       legtopmar <- 2
       legbotmar <- 0
     } else {
@@ -391,25 +425,56 @@ violinplot.default <- function(x, ..., at = NULL, width = 1, names = NULL,
   
   defplotargs <- list(xlim = xrange, ylim = yrange,
                       xaxt = "n", xaxs = "i", yaxt = "n",
-                      xlab = "", ylab = "Y", frame.plot = FALSE)
+                      xlab = "", ylab = "", frame.plot = FALSE,
+                      cex.lab = text.cex, cex.axis = text.cex)
+  if ("ylab" %in% names(dots) && ! is.null(dots$ylab)) {
+    ylab <- dots$ylab
+    dots$ylab <- NULL
+  }
   dots <- merge.list(dots, defplotargs)
 
+  ax <- NULL
   if (! add) {
     if (is.null(mar) || (is.logical(mar) && mar)) {
-      opar <- par(mar = c(legbotmar + group.labels + 1, 4, mainmar + legtopmar, 1) + 0.1)
+      opar <- par(mar = c(legbotmar + sum(group.labels) + 1, 4, mainmar + legtopmar, 1) + 0.1)
       on.exit(par(opar))
     }
 
     do.call("plot", c(list(x = NA), dots))
-    axis(2, las = 1)
+
+    if (! is.null(axis.args)) {
+      if (is.logical(axis.args) && axis.args) axis.args <- defaxisargs
+      axis.args <- merge.list(axis.args, defaxisargs)
+      ax <- do.call("axis", axis.args)
+    }
+
+    if (! is.null(ylab)) {
+      xs <- line2user(side = 2, line = 1) -
+        max(strwidth(as.character(ax), cex = text.cex)) -
+        strwidth("WW", cex = text.cex)
+      text(xs, mean(ax), labels = ylab, srt = 90, cex = text.cex, xpd = TRUE)
+    }
   }
   usr <- par('usr')
-  widen <- 0.4 * min(1, width)
+  widen <- 0.4 * min(1, if (width > 0) width else boxplot.args$pars$boxwex)
 
-  if (is.logical(grid.args)) grid.args <- if (grid.args) defgridargs else NULL
+  doVertGrid <- FALSE
+  if (is.logical(grid.args)) {
+    grid.args <- if (grid.args) defgridargs else NULL
+  } else if ("nx" %in% names(grid.args)) {
+    grid.args$nx <- NA
+    doVertGrid <- TRUE
+  }
   if (! is.null(grid.args)) {
     grid.args <- merge.list(grid.args, defgridargs)
     do.call("grid", grid.args)
+    if (doVertGrid) {
+      line.args <- grid.args[ names(grid.args) %in% c("col", "lwd", "lty") ]
+      y <- if (is.null(ax)) range(axTicks(side = 2)) else range(ax)
+      do.call("segments", c(list(x0 = at, y0 = rep(y[1], length(at)),
+                                 x1 = at, y1 = rep(y[2], length(at))),
+                            line.args))
+    }
   }
   tick.grid.args <- merge(grid.args, defgridargs)
   # remove these, we're only really interested in "col", "lwd", "lty", ...
@@ -417,9 +482,9 @@ violinplot.default <- function(x, ..., at = NULL, width = 1, names = NULL,
 
   col <- rep(if (is.null(col)) NA else col, length = nargs)
   border <- rep(if (is.null(border)) 1 else border, length = nargs)
-  denom <- (densmax / (width/2))
+  denom <- if (width > 0) (densmax / (width/2)) else boxplot.args$pars$boxwex
 
-  if (length(group.lines) == 1) group.lines <- rep(group.lines, nargs)
+  if (length(group.lines) == 1) group.lines <- rep(group.lines, length(groups))
 
   # this finds an "order of magnitude" for how to round-down the
   # densmax, going by 0.01 up to 0.25, 0.05 up to 0.5, 0.1 up to 2,
@@ -437,93 +502,74 @@ violinplot.default <- function(x, ..., at = NULL, width = 1, names = NULL,
 
     seght <- strheight("w", cex = text.cex) / 2
 
-    ngrps <- lengths(groups)
     ngrp1 <- prod(ngrps[-1])
     ngrps <- prod(ngrps)
 
-    for (i in seq_along(groups)) {
-      if (i > group.labels) next
-      mtext(names(groups)[i], side = 1, line = group.labels - i - 0.5,
-            at = line2user(line = 1, side = 2), adj = 1, cex = text.cex)
-      for (j in seq_len(length(groups[[i]]))) {
-        y <- line2user(line = group.labels - i, side = 1)
-        for (k in seq_along(groups[[i]][[j]])) {
-          dat <- groups[[i]][[j]][[k]]
-          mtext(names(groups[[i]])[j], side = 1, line = group.labels - i,
-                at = mean(dat), adj = 0.5, cex = text.cex)
-          if (group.lines[i]) {
-            lines(widen * c(-1, 1) + range(dat), rep(y, 2), xpd = TRUE)
+    # this is for the return value
+    ats <- lapply(groups, function(fctrs) {
+      lapply(fctrs, function(lvls) {
+        unname(sapply(split(lvls, c(0, cumsum(diff(lvls) - 1))),
+                      function(a) at[a]))
+      }) })
 
-            if (i == length(groups)) {
-              if (density.ticks || density.scale) {
-                if ((is.numeric(density.ticks) && 
-                       ((density.ticks > 0 && ((k-1)*ngrp1 + j) %in% density.ticks) ||
-                          (density.ticks < 0 && ((k-1)*ngrp1 + j - ngrps -1) %in% density.ticks))) ||
-                      (is.logical(density.ticks) && density.ticks)) {
-                  if (density.scale) {
-                    segments(dat[1] - c(-xtick, 0, xtick),
-                             rep(y, 3L),
-                             dat[1] - c(-xtick, 0, xtick),
-                             c(y + seght, y + seght / 4L, y + seght),
-                             xpd = TRUE)
-                    do.call("abline", c(list(v = dat[1] + c(-xtick, 0, xtick)),
-                                        tick.grid.args))
-                  } else {
-                    segments(dat[1] - widen * c(-1, 0, 1), rep(y, 3L),
-                             dat[1] - widen * c(-1, 0, 1), c(y + seght, y + seght / 4L, y + seght),
-                             xpd = TRUE)
-                  }
-                }
-              }
-              if (density.scale) {
-                if ((is.numeric(density.scale) && 
-                       ((density.scale > 0 && ((k-1)*ngrp1 + j) %in% density.scale) ||
-                          (density.scale < 0 && ((k-1)*ngrp1 + j - ngrps - 1) %in% density.scale))) ||
-                      (is.logical(density.scale) && density.scale)) {
-                  # text(dat[1] - widen, y, labels = sprintf("%0.0f%%", densmax * -100),
-                  #      adj = c(-0.1, 1.2), xpd = TRUE, cex = 0.7 * text.cex)
-                  text(dat[1] + xtick, y, labels = sprintf("%0.02f", ytick),
-                       adj = c(0.5, 1.2), xpd = TRUE, cex = 0.7 * text.cex)
-                }
-              }
-            } # if (i == length(groups))
-          } # if (group.lines[i])
-        } # for (k in seq_along(groups[[i]][[j]]))
-      } # for (j in seq_len(length(groups[[i]])))
-    } # for (i in seq_along(groups))
+    # ditto, though we use it internally as well
+    labels <- lapply(groups, function(g1) {
+      lapply(g1, function(g2) {
+        unname(sapply(split(g2, c(0, cumsum(diff(g2) - 1))),
+                      function(a) mean(at[a])))
+      }) })
+
+    # list with one elem for each factor
+    # each elem is list where:
+    # - names are the levels
+    # - content is locations of the levels within that level
+    # FIXME: put in example of labels using mtcars: mpg~cyl+vs
+    
+    # print all labels
+    ign <- sapply(1:ngrps, function(i) {
+      if (group.labels[i])
+        mapply(function(x, txt, ln, adj) mtext(txt, side = 1, line = ln, at = x,
+                                               adj = adj, cex = text.cex),
+               c(line2user(line = 0, side = 2), labels[[i]]),
+               c(groupnames[[i]], names(labels[[i]])),
+               sum(group.labels) - cumsum(group.labels)[i],
+               c(1, rep(0.5, length(labels[[i]]))))
+    })
 
   } # if (! is.null(groups))
 
-  for (i in seq_along(dens)) {
-    if (dens[[i]]$n < 2) {
-      thiscol <-
-        if (is.null(col[i])) {
-          if (is.null(border[i])) 'black' else border[i]
-        } else {
-          col[i]
-        }
-      points(rep(at[i], dens[[i]]$n), dens[[i]]$x, pch = 16, col = thiscol)
-    } else {
-      polygon(c(at[i] + dens[[i]]$y / denom,
-                at[i] - rev(dens[[i]]$y) / denom),
-              c(dens[[i]]$x, rev(dens[[i]]$x)),
-              col = col[i], border = border[i])
-    }
-  } # for (i in seq_along(dens))
+  if (width > 0) {
+    for (i in seq_along(dens)) {
+      if (dens[[i]]$n < 2) {
+        thiscol <-
+          if (is.null(col[i])) {
+            if (is.null(border[i])) 'black' else border[i]
+          } else {
+            col[i]
+          }
+        points(rep(at[i], dens[[i]]$n), dens[[i]]$x, pch = 16, col = thiscol)
+      } else {
+        polygon(c(at[i] + dens[[i]]$y / denom,
+                  at[i] - rev(dens[[i]]$y) / denom),
+                c(dens[[i]]$x, rev(dens[[i]]$x)),
+                col = col[i], border = border[i])
+      }
+    } # for (i in seq_along(dens))
+  } # if (width > 0)
 
   if (! is.null(legend.args)) {
     if (grepl("top", legpos)) {
       y <- line2user(line = 0, side = 3)
       yjust <- 0
     } else if (grepl("bottom", legpos)) {
-      y <- line2user(line = group.labels + 1.5, side = 1)
+      y <- line2user(line = sum(group.labels) + 1.5, side = 1)
       yjust <- 0.5
     } else {
       y <- mean(usr[3:4])
       yjust <- 0.5
     }
 
-    legtext <- c(names(groups)[ length(groups) ], legtext)
+    legtext <- c(groupnames[ ngrps ], legtext)
     fill <- c(NA, col[1:length(legtext)])
     border <- c(NA, rep(1, length(legtext)))
 
@@ -533,7 +579,7 @@ violinplot.default <- function(x, ..., at = NULL, width = 1, names = NULL,
         # group labels (on the bottom left)
         mtext(legtext[1],
               side = if (grepl("top", legpos)) 3 else 1,
-              line = if (grepl("top", legpos)) 0.5 else (group.labels + 1),
+              line = if (grepl("top", legpos)) 0.5 else (sum(group.labels) + 1),
               at = line2user(line = 1, side = 2),
               adj = 1
               )
@@ -579,7 +625,8 @@ violinplot.default <- function(x, ..., at = NULL, width = 1, names = NULL,
     bps <- do.call('boxplot', c(list(x = args, at = at, plot = FALSE), boxplot.args))
   } # if (doBP) ... else
 
-  invisible(list(at = at,
+  invisible(list(ats = ats,
+                 labels = labels,
                  xrange = xrange, yrange = yrange,
                  densmax = densmax,
                  boxplot = bps))
